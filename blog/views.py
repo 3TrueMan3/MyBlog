057fb441cee620448de372388d646812b18431e5
Michael <truemisha90@gmail.com>
2019-06-28 14:49:12 +0300

    Login Constraints
    Added restrictions for unauthorized users to create, update and delete posts and tags

--- blog/views.py
+++ blog/views.py
@@ -5,28 +5,31 @@ from django.urls import reverse
 from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
 
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
+    raise_exception = True
 
 
-class PostUpdate(UpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, UpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+    raise_exception = True
 
 
-class PostDelete(ObjectDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete_form.html'
     redirect_url = 'posts_list_url'
+    raise_exception = True
 
 
 class TagDetail(ObjectDetailMixin, View):
@@ -34,24 +37,25 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
 
 
-class TagUpdate(UpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, UpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+    raise_exception = True
 
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
 
     model = Tag
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tags_list_url'
-
-
+    raise_exception = True
 
 
 def posts_list(request):
